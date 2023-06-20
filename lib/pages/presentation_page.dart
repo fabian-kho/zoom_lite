9c8bf43e5b23b15eef61e2877d647b4c3c57cfe4
Dragnee1Natsu <116827545+Dragnee1Natsu@users.noreply.github.com>
2023-06-20 12:38:20 +0200

    <toggleOrientation gefixt

--- lib/pages/presentation_page.dart
+++ lib/pages/presentation_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pdf_render/pdf_render.dart';
 import 'package:pdf_render/pdf_render_widgets.dart';
 import 'package:firebase_database/firebase_database.dart';
@@ -71,6 +72,12 @@ class _PresentationPageState extends State<PresentationPage> {
   void toggleOrientation() {
     setState(() {
       isVerticalMode = !isVerticalMode;
+      if (!isVerticalMode) {
+        SystemChrome.setPreferredOrientations(
+            [DeviceOrientation.landscapeLeft]);
+      } else {
+        SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+      }
     });
   }
 
@@ -140,7 +147,7 @@ class _PresentationPageState extends State<PresentationPage> {
                 child: Container(
                   margin: const EdgeInsets.symmetric(horizontal: 10),
                   child: AspectRatio(
-                    aspectRatio: isVerticalMode ? 16 / 9 : 9 / 16,
+                    aspectRatio: isVerticalMode ? 16 / 9 : 16 / 9,
                     child: PdfDocumentLoader.openFile(
                       widget.filePath,
                       onError: (err) => print(err),
